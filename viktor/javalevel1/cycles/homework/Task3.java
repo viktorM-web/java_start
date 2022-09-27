f3109709cf8dca19cbbf894e321b9c8f6afcba09
viktorM-web <81855790+viktorM-web@users.noreply.github.com>
2022-09-27 13:11:08 +0300

    add files with homework by String

---
+++ viktor/javalevel1/cycles/homework/Task3.java
@@ -0,0 +1,39 @@
+package com.viktor.javalevel1.cycles.homework;
+
+/**
+ * Программист Ваня сразу после окончания курсов dmdev устроился в IT компанию на позицию Junior Java Developer с зарплатой 600$ в месяц.
+ * Ему обещали, что будут поднимать зарплату на 400$ каждые 6 месяцев.
+ * 300$ в месяц Ваня тратит на еду и развлечения.
+ * 10% от зарплаты Ваня ежемесячно переводит на счет брокера, чтобы инвестировать в акции с доходностью 2% в месяц.
+ * <p>
+ * Посчитать, сколько Ваня будет иметь средств на своем счету и на счету брокера за 3 года и 2 месяца.
+ * <p>
+ * Для интереса: попробовать увеличить процент, которые Ваня инвестирует из своей зарплаты
+ */
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        double investPercent = 0.1;
+        solution(investPercent);
+    }
+
+    private static void solution(double investPercent) {
+        double profitPercent = 0.02;
+        int salary = 600;
+        int spending = 300;
+        double brokerAccount = 0;
+        double bankAccount = 0;
+        int periodInMonth = 38;
+        for (int i = 1; i <= periodInMonth; i++) {
+            bankAccount += salary - salary * investPercent;
+            bankAccount -= spending;
+            brokerAccount = brokerAccount + brokerAccount * profitPercent + salary * investPercent;
+            if (i % 6 == 0) {
+                salary += 400;
+            }
+            System.out.println("In " + i + " month bankAccount " + bankAccount + "$" + " brokerAccount " + brokerAccount + "$");
+        }
+        System.out.println("RESULT:\n" + brokerAccount + "$ on Broker account\n" + bankAccount + "$ on Bank account");
+    }
+}
---
+++ viktor/javalevel2/beginningoop/homework/Runner.java
@@ -0,0 +1,35 @@
+package com.viktor.javalevel2.beginningoop.homework;
+
+public class Runner {
+    public static void main(String[] args) {
+        Room passageRoom = new Room(true);
+        Room notPassageRoom = new Room(false);
+        Flat firstFlat = new Flat(1, new Room[]{passageRoom, passageRoom, notPassageRoom});
+        Flat secondFlat = new Flat(2, new Room[]{passageRoom, passageRoom});
+        Flat thirdFlat = new Flat(3, new Room[]{passageRoom, passageRoom});
+        Flat fourthFlat = new Flat(4, new Room[]{passageRoom, passageRoom, notPassageRoom});
+        Flat fifthFlat = new Flat(5, new Room[]{passageRoom, passageRoom});
+        Flat sixthFlat = new Flat(6, new Room[]{passageRoom, passageRoom, notPassageRoom});
+        Floor firstFloor = new Floor(1, new Flat[]{firstFlat});
+        Floor secondFloor = new Floor(2, new Flat[]{secondFlat, thirdFlat, fourthFlat});
+        Floor thirdFloor = new Floor(3, new Flat[]{fifthFlat, sixthFlat});
+        Building building = new Building(160, new Floor[]{firstFloor, secondFloor, thirdFloor});
+        printAllInformation(building);
+    }
+
+    private static void printAllInformation(Building building) {
+        building.print();
+        for (Floor floor : building.getNumberOfStoreys()) {
+            System.out.print("  ");
+            floor.print();
+            for (Flat flat : floor.getPlanFloor()) {
+                System.out.print("      ");
+                flat.print();
+                for (Room room : flat.getPlanFlat()) {
+                    System.out.print("          ");
+                    room.print();
+                }
+            }
+        }
+    }
+}
