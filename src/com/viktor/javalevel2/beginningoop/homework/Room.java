d363de58edac84be104d84428c4722792103612b
viktorM-web <81855790+viktorM-web@users.noreply.github.com>
2022-09-27 13:14:06 +0300

    add homework by String

---
+++ src/com/viktor/javalevel2/beginningoop/homework/Room.java
@@ -0,0 +1,15 @@
+package com.viktor.javalevel2.beginningoop.homework;
+
+public class Room {
+    private boolean isPassage;
+
+    Room(boolean isPassage) {
+        this.isPassage = isPassage;
+    }
+
+    public void print() {
+        if (isPassage) {
+            System.out.println("Passage room");
+        } else System.out.println("Not Passage room");
+    }
+}
